324cb584d8063ae5630d88c68ff24f44163e678a
Verta-master <vertyagina@gmail.com>
2020-12-23 13:07:26 +0600

    Header

---
+++ js/catalog-slider.js
@@ -0,0 +1,38 @@
+var breakpoint = window.matchMedia('(max-width: 1022px)');
+var newsSwiper;
+var breakpointChecker = function() {
+  if (breakpoint.matches === true) {
+    if (newsSwiper !== undefined) newsSwiper.destroy(true, true);
+    return;
+    } else if (breakpoint.matches === false) {
+      return enableSwiper();
+    }
+};
+
+var enableSwiper = function() {
+  newsSwiper = new Swiper ('.catalog__slider', {
+    navigation: {
+      nextEl: '.catalog__next',
+      prevEl: '.catalog__prev',
+    },
+    simulateTouch: false,
+    preventClicks: false,
+    preventClicksPropagation: false,
+    slidesPerView: 2,
+    spaceBetween: 20,
+    breakpoints: {
+      1170: {
+        slidesPerView: 3,
+      },
+      766: {
+        slidesPerView: 2,
+      },
+      480: {
+        slidesPerView: 1,
+      }
+    }
+  })
+};
+
+breakpoint.addListener(breakpointChecker);
+breakpointChecker();
